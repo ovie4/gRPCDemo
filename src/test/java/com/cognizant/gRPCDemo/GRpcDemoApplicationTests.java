deb09dbec0db51a67c25ec2bbaa7667ab196fcba
Ovie <you@example.com>
2020-08-13 09:24:20 -0400

    initial gRPC server files autogenerated

---
+++ src/test/java/com/cognizant/gRPCDemo/GRpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.gRPCDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
